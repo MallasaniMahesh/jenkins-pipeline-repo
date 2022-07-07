d6da31bc4f416e0610a4b683c73f9ff82cb2922d
Mallasani Maheh <58106946+MallasaniMahesh@users.noreply.github.com>
2022-07-07 08:55:10 -0500

    Update handler.py
    second commit

--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v3.0! Demo pipeline is successfully deployed test1",
+        "message": "Go Serverless v3.0! Demo pipeline is successfully deployed test2",
         "input": event,
     }
 
